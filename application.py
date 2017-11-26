17e7293efe0b1e5e183661a6da3967a8e137ee4f
Dipti Pandey <dcdeepti03@gmail.com>
2017-11-26 19:30:14 +0000

    Adding flask server and index file

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
