5d96114d1f5dd45a5994dc787d19d01bea89cf99
Dipti Pandey <dcdeepti03@gmail.com>
2017-12-06 00:48:56 +0000

    add script to denote active tab

--- static/scripts.js
+++ static/scripts.js
@@ -13,3 +13,17 @@ function archiveMessage(message_id) {
         error: function() { alert('An error occurred');}
     });
 }
+
+$(document).ready(function(){
+    var currentLocation = window.location.href;
+    var home = $("#home");
+    var register = $("#register");
+    var notify = $("#notify");
+    if(currentLocation.indexOf("register-donor") !== -1){
+        register.addClass("active");
+    } else if(currentLocation.indexOf("notify-donors") !== -1) {
+        notify.addClass("active");
+    } else {
+        home.addClass("active");
+    }
+});
